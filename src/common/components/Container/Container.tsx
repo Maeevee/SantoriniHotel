195b18c7da9f0d6b99b46b81e1f4f89c96a9b7cd
Mariia Kiriakulova <mariya.kiryakulova@gmail.com>
2024-01-31 14:07:36 +0000

    fix-container

--- src/common/components/Container/Container.tsx
+++ src/common/components/Container/Container.tsx
@@ -1,6 +1,17 @@
 
-interface IProps{
-    children: React.ReactNode
+// interface IProps{
+//     children: React.ReactNode
+// }
+
+// export const Container = ({children}:IProps) => <div className="p-8">{children}</div>
+
+
+
+interface IProps {
+    children: React.ReactNode;
+    className?: string;
 }
 
-export const Container = ({children}:IProps) => <div className="p-8">{children}</div>
+export const Container = ({ children, className }: IProps) => (
+    <div className={`p-8 ${className}`}>{children}</div>
+);
