9cb37f6d6bc934e73b3c9aa00b9de999c05f2818
Mariia Kiriakulova <mariya.kiryakulova@gmail.com>
2024-02-08 16:39:40 +0000

    add-styles-to-AnimatedSection

--- src/features/pages/HomePage/components/AnimatedSection/AnimatedSection.tsx
+++ src/features/pages/HomePage/components/AnimatedSection/AnimatedSection.tsx
@@ -11,16 +11,13 @@ export const AnimatedSection = () => {
     }
 
     return(
-            <>
-                <div className="mt-40">
-                    <Header>If you have any questions about our rooms, amenities, or services, please don't hesitate to ask.</Header>
-                    <p>We're here to help make your stay as comfortable and enjoyable as possible.</p>
-                </div>
-                <form>
-                    <input placeholder="Email" required value={mail} onChange={handleInputChange}/>
-                    <textarea placeholder="Your question" />
-                    <button type="submit">SEND</button>
+            <div className="px-9 pt-12 pb-14 mb-28 rounded-2xl mt-40 bg-[url('/images/animatedsection/Rectangle15.png')] bg-no-repeat text-white">
+                    <Header className="mb-20 w-[70%]">If you have any questions about our rooms, amenities, or services, please don't hesitate to ask.</Header>
+                <form className="flex flex-col w-[28%]">
+                    <input className="text-black rounded-[20px] h-14 px-4" placeholder="Email" required value={mail} onChange={handleInputChange}/>
+                    <textarea className="text-black rounded-[20px] h-36 mt-4 mb-10 p-4 resize-none"  placeholder="Your question" />
+                    <button className="bg-primary rounded-[20px] h-14 w-100" type="submit">SEND</button>
                 </form>
-            </>
+            </div>
     )
 }
