2212b9391e1fe87402cf54bd32710acaf2abba4a
Mariia Kiriakulova <mariya.kiryakulova@gmail.com>
2024-02-08 20:31:57 +0000

    add-scroll-bg-to-nav

--- src/features/pages/HomePage/components/Banner/Banner.tsx
+++ src/features/pages/HomePage/components/Banner/Banner.tsx
@@ -2,16 +2,34 @@ import { Container } from "@/common/components/Container/Container"
 import { CheckAvailability } from "./components/CheckAvailability/CheckAvailability"
 import {useBannerNav} from "./hooks/useBannerNav"
 import { Navigation } from "@/common/components/Navigation/Navigation"
+import { useEffect, useState } from "react"
+import { motion } from "framer-motion";
 // import styles from "./styles.css"
 
 export const Banner = () => {
 
     const bannerNav = useBannerNav()
 
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 120) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+    
+        window.addEventListener('scroll', onScroll);
+    
+        return () => window.removeEventListener('scroll', onScroll);
+    }, []);
+
     return(
-        <div className="bg-[url('/images/banner/Rectangle20.png')] bg-no-repeat text-white">
+        <div className="bg-[url('/images/banner/a370b02e6a33d60e3d82a535331b79a0.png')] bg-no-repeat bg-center bg-cover text-white">
                 <header className="relative h-20">
-                <Navigation classNameUl={"fixed flex px-9 py-6 w-full items-center justify-center text-sm space-x-10 [&>*:nth-child(3)]:uppercase [&>*:nth-child(3)]:w-full [&>*:nth-child(3)]:text-center [&>*:nth-child(3)]:font-bold [&>*:nth-child(3)]:text-xl [&>*:nth-child(4)]:text-nowrap"} footer={false}/>
+                <Navigation classNameUl={`fixed z-50 transition ease-in-out duration-700 flex px-9 py-6 w-full items-center justify-center text-sm space-x-10 [&>*:nth-child(3)]:uppercase [&>*:nth-child(3)]:w-full [&>*:nth-child(3)]:text-center [&>*:nth-child(3)]:font-bold [&>*:nth-child(3)]:text-xl [&>*:nth-child(4)]:text-nowrap [&>*]:opacity-100 [&>*:hover]:opacity-60 ${scrolled ? "bg-black" : ""}`} footer={false}/>
                     {/* <nav className="fixed">
                         <ul className="flex items-center justify-center text-sm space-x-10 [&>*:nth-child(3)]:uppercase [&>*:nth-child(3)]:w-full [&>*:nth-child(3)]:text-center [&>*:nth-child(3)]:text-xl">
                             {bannerNav.map(({id, title, className}) => <li className={className} key={id}>{title}</li>)}
